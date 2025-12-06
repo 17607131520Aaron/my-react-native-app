88f4c84ba4a23100cc0f3cba8bba9cfb373975dc
v-huangyafeng3 <v-huangyafeng3@xiaomi.com>
2025-12-06 16:18:43 +0800

    feat: init

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LoginPage = () => {
+  return (
+    <View>
+      <Text>登录页面</Text>
+    </View>
+  );
+};
+
+export default LoginPage;
---
+++ src/pages/ScanInboundPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ScanInboundPage = () => {
+  return (
+    <View>
+      <Text>扫描入库</Text>
+    </View>
+  );
+};
+
+export default ScanInboundPage;
